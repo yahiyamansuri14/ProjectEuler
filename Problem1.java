73e28ba47402f694e51eebcb1abedf7859ccf7f1
yahiyamansuri14 <52306538+yahiyamansuri14@users.noreply.github.com>
2019-07-26 10:56:31 +0530

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -4,18 +4,12 @@
 Find the sum of all the multiples of 3 or 5 below 1000.*/
 class Problem1{
 	public int sum(){
-		int sum1=0,sum2=0;
-		
+		int sum=0;
 		for(int i=0;i<1000;i++){
-			if (i%3==0)
-					sum1=sum1+i;
-
-			if (i%5==0 && i%3!=0)
-					sum2=sum2+i;
-						
+			if(i%3==0 || i%5==0)
+				sum=sum+i;
 		}
-		return (sum1+sum2);
-		
+		return sum;
 	}
 	
 	public static void main(String args[]){
